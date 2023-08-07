34edb0a436daa71fb7ac56e1410a2d5976ea8072
h9661 <guscksdn123@gmail.com>
2023-08-07 17:46:50 +0900

    middleware

--- src/index.js
+++ src/index.js
@@ -3,32 +3,41 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-
-let groceryList = 
-[
-    {
-        item: 'milk',
-        quantity: 2,
-    },
-    {
-        item: 'cereal',
-        quantity: 2,
-    },
-    {
-        item: 'cereal',
-        quantity: 2,
-    },
-]
-
-app.get('/groceries', (req, res) => {
-    res.send(groceryList);
+app.use((req, res, next) => {
+    console.log(`${req.method}:${req.url}`);
+    next();
 })
 
-app.post('/groceries', (req, res) => {
+let groceryList = [
+    {
+        item: "milk",
+        quantity: 2,
+    },
+    {
+        item: "cereal",
+        quantity: 2,
+    },
+    {
+        item: "cereal",
+        quantity: 2,
+    },
+];
+
+app.get(
+    "/groceries",
+    (req, res, next) => {
+        console.log("before handling get request");
+        next();
+    },
+    (req, res, next) => {
+        res.send(groceryList);
+    }
+);
+
+app.post("/groceries", (req, res, next) => {
     groceryList.push(req.body);
     res.sendStatus(201);
-})
-
+});
 
 app.listen(port, () => {
     console.log(`running server. https://localhost:${port}`);
