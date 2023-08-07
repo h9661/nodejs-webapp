9f9cdb09abd7be16edb68d60c3592b77fda203e5
h9661 <guscksdn123@gmail.com>
2023-08-07 17:28:43 +0900

    post

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 - [x] express가 뭔지 학습
 - [x] 간단하게 서버 만들어보기.
-- [ ] 방명록 만들어보기
+
+
+# 학습 정리
+1. middleware
+https://velog.io/@wjddnjswjd12/node.js-express-%EB%AF%B8%EB%93%A4%EC%9B%A8%EC%96%B4%EB%9E%80
+
+2. get
+
+3. post
+
+https://www.youtube.com/watch?v=o4RLiTIOfhQ&list=PL_cUvD4qzbkwp6pxx27pqgohrsP8v1Wj2&index=4&ab_channel=AnsontheDeveloper
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/groceries', (req, res) => {
-    res.send([
+app.use(express.json());
+
+let groceryList = 
+[
     {
         item: 'milk',
         quantity: 2,
@@ -16,8 +18,17 @@ app.get('/groceries', (req, res) => {
         item: 'cereal',
         quantity: 2,
     },
-])
+]
+
+app.get('/groceries', (req, res) => {
+    res.send(groceryList);
 })
+
+app.post('/groceries', (req, res) => {
+    groceryList.push(req.body);
+    res.sendStatus(201);
+})
+
 
 app.listen(port, () => {
     console.log(`running server. https://localhost:${port}`);
