1b42c8cea2be6a989a477b230f17aff09de8c4c0
h9661 <guscksdn123@gmail.com>
2023-08-15 00:21:02 +0900

    정규표현식을 통해 register 변수들을 검증한다.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -10,13 +10,35 @@ async function authRegisterController(req, res) {
         res.status(400);
         res.send("user already exists!");
     } else {
-        const hashedPassword = hashPassword(password);
-        User.create({
-            username,
-            password: hashedPassword,
-            email,
-        });
-        res.status(201);
+        if (!/^([a-zA-Z]+){4,12}$/.test(username)) var usernameError = true;
+
+        if (!/^([a-zA-Z0-9]+){4,12}$/.test(password)) var passwordError = true;
+
+        if (!/^[a-zA-Z0-9]+\@[a-zA-Z]+\.[a-zA-Z]+$/.test(email)) var emailError = true;
+
+        if (usernameError || passwordError || emailError) {
+            req.session.registerError = {
+                usernameError,
+                passwordError,
+                emailError,
+            };
+
+            res.redirect("http://localhost:3000/api/v1/auth/register");
+            return true;
+        } else {
+            const hashedPassword = hashPassword(password);
+
+            const newUser = await User.create({
+                username,
+                password: hashedPassword,
+                email,
+            });
+            console.log(newUser);
+            console.log("new user created!");
+
+            res.redirect("http://localhost:3000/api/v1/auth/login");
+            return true;
+        }
     }
 }
 
