b085a06a015ac975d14e36f679456d0896dfc354
h9661 <guscksdn123@gmail.com>
2023-08-17 13:09:29 +0900

    item으로 검색하는 기능 만들었음

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,20 @@ const router = Router();
 
 router.get("/", async (req, res) => {
     let userId = req.user.id;
-
     let user = await User.findById(userId);
-    console.log(user);
-    res.render("main.ejs", {groceries: user.groceries});
+    let { filterItem } = req.query;
+
+    if (filterItem) {
+        const regexForFilterItem = new RegExp(filterItem);
+
+        let groceries = user.groceries.filter((g) =>
+            regexForFilterItem.test(g.item)
+        );
+
+        res.render("main.ejs", { groceries: groceries });
+    } else {
+        res.render("main.ejs", { groceries: user.groceries });
+    }
 });
 
 module.exports = {
